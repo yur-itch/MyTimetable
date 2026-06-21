0d0ef08b0dd60cc12dac69d48a0d42e3f1f4b440
yurich <urvancevurij24@gmail.com>
2026-06-22 00:55:28 +0700

    Добавьте файлы проекта.

---
+++ MyTimetable/Controllers/AppController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyTimetable.Controllers
+{
+    [Route("[controller]")]
+    public class AppController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return View();
+        }
+    }
+}
