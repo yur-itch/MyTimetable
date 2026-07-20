6632ba34509a1fa41f5dac5a0cd454807faa40ee
yurich <urvancevurij24@gmail.com>
2026-07-21 06:46:32 +0700

    auto: write C:/Users/PCyur/source/repos/MyTimetable/bench_brotli.cs

--- MyTimetable/Models/PageView.cs
+++ MyTimetable/Models/PageView.cs
@@ -13,6 +13,6 @@ namespace MyTimetable.Models
     public sealed record TableView {
         public required int SlotCount { get; init; }
         public required string ScrollTarget { get; init; }
-        public required IReadOnlyList<string> Days { get; init; }
+        public required string Table { get; init; }
     }
 }
