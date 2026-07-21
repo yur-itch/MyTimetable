cdf84e97e24be6b93bd5576d5a10e067f60d1ca7
yurich <urvancevurij24@gmail.com>
2026-07-21 07:23:25 +0700

    auto: edit C:/Users/PCyur/source/repos/MyTimetable/MyTimetable/Controllers/CliController.cs

--- MyTimetable/Controllers/CliController.cs
+++ MyTimetable/Controllers/CliController.cs
@@ -38,13 +38,8 @@ namespace MyTimetable.Controllers
             {
                 return StatusCode(503, "Расписание временно недоступно");
             }
-            string resp = JsonSerializer.Serialize(new
-            {
-                slotCount = _data.CliSlotCount,
-                scrollTarget = _data.CliScrollTarget,
-                data = _data.CliViewResult
-            });
-            return File(resp, "application/json; charset=utf-8");
+            Response.Headers.ContentEncoding = "br";
+            return File(_data.CliViewResult, "application/json; charset=utf-8");
         }
 
         public record LoginRequest(string Username, string Password);
