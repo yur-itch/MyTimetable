9a739035bdcb326d5041fbf50ada28e22ed71aa9
yurich <urvancevurij24@gmail.com>
2026-07-24 10:37:10 +0700

    auto: agent edit C:/Users/PCyur/source/repos/MyTimetable/MyTimetable/Entities/Session.cs

--- MyTimetable/Entities/Session.cs
+++ MyTimetable/Entities/Session.cs
@@ -4,7 +4,7 @@ namespace MyTimetable.Entities
     public class Session
     {
         [Key]
-        public string Id { get; set; }
+        public string Id { get; set; } = null!;
         public required string Username { get; set; }
         public required DateTime StartedAt { get; set; }
         public required DateTime ExpiresAt { get; set; }
