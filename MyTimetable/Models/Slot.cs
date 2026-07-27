66fcdc4a9898b3ec2f73ccff6079c46a2a747dc6
yurich <urvancevurij24@gmail.com>
2026-07-27 17:10:56 +0700

    auto: agent edit MyTimetable/Models/Slot.cs

--- MyTimetable/Models/Slot.cs
+++ MyTimetable/Models/Slot.cs
@@ -1,5 +1,5 @@
 namespace MyTimetable.Models
 {
-    public readonly record struct Slot(DateOnly Date, int Number)
+    public readonly record struct Slot(DateOnly Date, int Number);
     //public class AvailableSlot : Slot { public AvailableSlot(DateOnly date, int number) : base(date, number) { } }
 }
